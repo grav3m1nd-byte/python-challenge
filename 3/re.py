a9caad155430669e793ff4137ecf34669e357edc
use <usquared@gmail.com>
2009-07-24 10:22:05 -0700

    3 solved - problem was poorly worded, since i was matching "exactly" three large letters, but it should have said "exactly tree and not more than 3"

--- 3/re.py
+++ 3/re.py
@@ -1,1 +1,18 @@
+import re
+
 f = open('text.txt')
+o = open('out.txt','w')
+
+filestr = ''
+
+for line in f:
+    filestr += line[:len(line)-1]
+
+miter = re.finditer('(?<![A-Z])[A-Z]{3}([a-z])[A-Z]{3}(?![A-Z])',filestr)
+
+outstr = ''
+
+for m in miter:
+    outstr += m.group(1)
+
+print outstr

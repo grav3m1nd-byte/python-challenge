5500b706c5a23616360ce6f0b89cb7e2afc5f2d7
use <usquared@gmail.com>
2009-07-25 11:50:12 -0700

    6 finished with detailed notes

--- 6/zipfiles.py
+++ 6/zipfiles.py
@@ -1,1 +1,24 @@
-import zipfile
+import zipfile,re
+
+
+
+z = zipfile.ZipFile('channel.zip')
+n = '90052' ;
+
+solution = False
+comments = ''
+
+while not solution:
+    f = z.open(n+'.txt')
+    comments += z.getinfo(n+'.txt').comment
+    
+    fstr = f.read()
+    match = re.search('Next nothing is (\d+)',fstr)
+    if match:
+        #print n
+        n = match.group(1)
+    else:
+        solution = fstr
+        print fstr
+
+print comments

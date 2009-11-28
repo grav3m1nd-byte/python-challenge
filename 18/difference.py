b90a37768f964ad80edc277c4a0829326b7116cd
unknown <w@.(none)>
2009-11-28 12:18:15 -0800

    on the right track with 18

--- 18/difference.py
+++ 18/difference.py
@@ -5,11 +5,13 @@ im = Image.open('balloons.jpg')
 im1 = im.crop((0,0,375,335))
 im2 = im.crop((375,0,750,335))
 
-im1.save('lighter.jpg')
-im2.save('darker.jpg')
+#im1.save('lighter.jpg')
+#im2.save('darker.jpg')
 
-difference = ImageChops.difference(im1,im2)
-subtraction = ImageChops.subtract(im1,im2)
-
-difference.show()
-subtraction.show()
+limit = 2
+for i in range(0,limit):
+    diff = ImageChops.difference(im1,im2)
+    diff.save(str(i)+'.jpg')
+    diff.show()
+    im1 = im2
+    im2 = diff
